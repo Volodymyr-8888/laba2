592ee3353aa571f38321b3570a1c9d1dcb0da008
Volodymyr <Volodymyr_Kibyuk93@ukr.net>
2024-11-05 21:13:25 +0200

    діапазон

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import random
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    rand = random.randint(1,6)
-    txt = f"Випадковe число від 1 до 6: {rand}"
+    rand = random.randint(1,10)
+    txt = f"Випадковe число від 1 до 10: {rand}"
     return txt
